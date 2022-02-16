893ad4159a85b39db99a65209f7d10f02841afaf
niyumi <99313696+IT21278730@users.noreply.github.com>
2022-02-16 20:04:31 +0000

    I did all the things in pervious tute this is the right one

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,21 @@
-/* Exercise 1 - Calculations
+#include<stdio.h>
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+int main()
 
-#include <stdio.h>
-
-int main() {
-  
-  return 0;
+{
+	int mark1 , mark2; //define variables
+	float avg;
+	
+	printf("Enter the first mark :");  //Ask user to input mark1
+	scanf("%d" , &mark1);
+	
+	printf("Enter the second mark :"); //ask uer to input mark2
+	scanf("%d" , &mark2);
+	
+	avg = (float)(mark1 + mark2)/2;   //Calculation for average
+	
+	printf("Average : %.2f" , avg);   //printing average
+	
+	return 0;  //End of the process
+	
 }
-
