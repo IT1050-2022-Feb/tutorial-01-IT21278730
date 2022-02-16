76256d397520c4d28f7da9aae4ef2043a2f4c83f
niyumi <99313696+IT21278730@users.noreply.github.com>
2022-02-16 20:06:09 +0000

    completed tute 2

--- Tute02.c
+++ Tute02.c
@@ -1,24 +1,20 @@
-/*Exercise 2 - Selection
-
-Write a program to calculate the amount to be paid for a rented vehicle.
-
-•	Input the distance the van has travelled
-•	The first 30 km is at a rate of 50/= per km.
-•	The remaining distance is calculated at the rate of 40/= per km.
-
-
-e.g.
-
-Distance -> 20
-Amount = 20 x 50 = 1000
-
-
-Distance -> 50
-Amount = 30 x 50 + (50-30) x 40 = 2300*/
-
 #include <stdio.h>
-
 int main() {
   
+  int i , n , a;  //define variables
+  int sum;
+
+
+  printf("Enter a value for n :");   //ask user to input value for n
+  scanf("%d" , &n);
+
+  for(i=1 ; i <= n ; i++)
+  {
+    sum += i;  // calculation
+
+  }
+
+      printf("\nSum = %d" , sum);  //print sum
+
   return 0;
 }
